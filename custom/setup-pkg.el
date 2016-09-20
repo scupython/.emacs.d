108e1a55dd1470c1d61051b4478ded5d7f5afe35
xhorn pan <scupython@gmail.com>
2016-09-19 22:13:21 -0400

    from scrath init

---
+++ custom/setup-pkg.el
@@ -0,0 +1,14 @@
+(defconst required-pkgs
+  '(dracula-theme
+    yasnippet
+    company
+    company-irony
+    company-irony-c-headers
+    rtags
+    irony
+    flycheck
+    flycheck-irony
+    cmake-ide
+    clean-aindent-mode
+    
+    ))
